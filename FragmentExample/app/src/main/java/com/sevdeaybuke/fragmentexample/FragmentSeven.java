2a1fe9252373f1926be5840874a116e9ae0b9d58
Sevde Aybüke Kaleli <sevdeaybuke@gmail.com>
2019-04-11 21:49:05 +0300

    First Commit
    Commit

---
+++ FragmentExample/app/src/main/java/com/sevdeaybuke/fragmentexample/FragmentSeven.java
@@ -0,0 +1,23 @@
+package com.sevdeaybuke.fragmentexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentSeven extends Fragment {
+    public FragmentSeven() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_seven, container, false);
+    }
+}
